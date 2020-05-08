719dcd8404b88fa1943524c0452eb7074609a45b
skouroufexis <skouroufexis@gmail.com>
2020-05-09 00:48:56 +0300

    Update directors.js

--- directors.js
+++ directors.js
@@ -24,35 +24,35 @@ let directors=[
 
 {"id":"4",
  "name":"Bong Joon Ho",
- "biogrphy":"Bong Joon-ho (Korean: 봉준호, born September 14, 1969) is a South Korean filmmaker. His films feature social themes, genre-mixing, black humor, and sudden tone shifts. In 2017, Metacritic ranked Bong 13th on its list of the 25 best film directors of the 21st century.",
- "birth":"September 14, 1969 ",
+ "biogrphy":"Bong Joon-ho is a South Korean filmmaker. His films feature social themes, genre-mixing, black humor, and sudden tone shifts. In 2017, Metacritic ranked Bong 13th on its list of the 25 best film directors of the 21st century.",
+ "birth":"September 14, 1969 ", 
  "death":""
 },
 
 {"id":"5",
  "name":"Tassos Boulmetis",
  "biogrphy":"Tassos Boulmetis studied Physics at the University of Athens and Film Production and Direction at the University of California (UCLA). He started his career as a director/producer of TV shows for the state TV networks.",
- "birth":"1957",
+ "birth":"1957", 
  "death":""
 },
 
 {"id":"6",
  "name":"Florian Henckel von Donnersmarck",
  "biogrphy":"Florian Maria Georg Christian Graf Henckel von Donnersmarck (born 2 May 1973) is a German film director, best known for writing and directing the 2006 Oscar-winning dramatic thriller The Lives of Others, the 2010 three time Golden Globe nominated romantic thriller The Tourist starring Angelina Jolie and Johnny Depp, and the two-time Oscar-nominated 2018 epic drama Never Look Away",
- "birth":"2 May 1973",
+ "birth":"2 May 1973", 
  "death":""
 },
 
 {"id":"7",
  "name":"Sergio Leone",
  "biogrphy":"Sergio Leone (January 3, 1929 – April 30, 1989) was an Italian film director, producer and screenwriter, credited as the creator of the Spaghetti Western genre and widely regarded as one of the most influential directors in the history of cinema.",
- "birth":"January 3, 1929",
- "death":"April 30, 1989"
+ "birth":"January 3, 1929", 
+ "death":"April 30, 1989" 
 },
 
 {"id":"8",
  "name":"Oliver Hirschbiegel",
- "biogrphy":"Oliver Hirschbiegel (born 29 December 1957)is a German film director. His works include Das Experiment and the Oscar-nominated Downfall.",
+ "biogrphy":"Oliver Hirschbiegel is a German film director. His works include Das Experiment and the Oscar-nominated Downfall.",
  "birth":"December 29, 1957",
  "death":""
 },
@@ -60,7 +60,7 @@ let directors=[
 {"id":"9",
  "name":"Andrey Kravchuk",
  "biogrphy":"Andrei Yurievich Kravchuk (Russian: Андре́й Ю́рьевич Кравчу́к; born 13 April 1962 in Leningrad) is a Russian television and film director and screenwriter best known for his films The Italian (2005) and The Admiral (2008).",
- "birth":"April 13, 1962",
+ "birth":"April 13, 1962", 
  "death":""
 },
 
