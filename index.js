1fcba3f03e78eac5dd2643c1f54084e3ddd9c8e4
skouroufexis <skouroufexis@gmail.com>
2020-05-25 17:25:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const { check, validationResult } = require('express-validator');
 
 //database connection
 // mongoose.connect('mongodb://localhost:27017/myFlix', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
 
 
 
