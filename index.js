538ea234714f4e6b0aaf9e5eca3c9b4284564cef
skouroufexis <skouroufexis@gmail.com>
2020-05-30 13:12:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ require('./auth.js')(app);
 
 //routes
 app.get('/' , function(request,response){
-    response.sendFile(__dirname+'documentation.html');
+    response.sendFile(__dirname+'/public/documentation.html');
     }
 )
 
