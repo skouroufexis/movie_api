d8f9795e03bf6ebc239c152cb0ff29719ed208d2
skouroufexis <skouroufexis@gmail.com>
2020-06-04 18:36:02 +0300

    .

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,22 +1,39 @@
 import React from 'react';
 import axios from 'axios';
+import { MovieCard } from '../movie-card/movie-card';
 
 
-class MainView extends React.Component {
-    constructor() {
-      // Call the superclass constructor
-      // so React can initialize it
-      super();
-  
-      // Initialize the state to an empty object so we can destructure it later
-      this.state = {};
-    }
-  
-    // This overrides the render() method of the superclass
-    // No need to call super() though, as it does nothing by default
-    render() {
-      return (
-       <div className="main-view">aa</div>
-      );
-    }
+export class MainView extends React.Component {
+
+  // One of the "hooks" available in a React Component
+  componentDidMount() {
+    axios.get('https://stavflix.herokuapp.com/movies')
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
+
+
+  render() {
+    // If the state isn't initialized, this will throw on runtime
+    // before the data is initially loaded
+    const { movies } = this.state;
+
+    // Before the movies have been loaded
+    if (!movies) return <div className="main-view"/>;
+
+    return (
+     <div className="main-view">
+     { movies.map(movie => (
+       <div className="movie-card" key={movie._id}>{movie.Title}</div>
+     ))}
+     </div>
+    );
+  }
+}
