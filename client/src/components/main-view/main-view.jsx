97e4bae611224b1d2ed3121447007b830bf7337b
skouroufexis <skouroufexis@gmail.com>
2020-06-27 18:16:08 +0300

    add login and register views to "main view"

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,73 +1,130 @@
 import React from 'react';
 import axios from 'axios';
-import {MovieCard, BtnMovieCard} from '../movie-card/movie-card';
 
+import './main-view.scss';
+
+import {Container, Row, Col} from 'react-bootstrap';
+
+import {LoginView} from '../login-view/login-view';
+import {RegistrationView} from '../registration-view/registration.view';
+import {MovieCard, BtnMovieCard} from '../movie-card/movie-card';
 import {MovieView} from '../movie-view/movie-view';
 
 class MainView extends React.Component{
   constructor(){
     super();
-    this.state ={content:null,selected:null};
+    this.state ={content:null,selected:null,isLogged:null,openRegister:null};
+    
+    
   }
 
   render(){
     
-    var movies = this.state.content;
-    var selected=this.state.selected;
-    var back = this.state.back;
+    let islogged=this.state.isLogged;
+    let openregister=this.state.openRegister;
+    let movies = this.state.content;
+    let selected=this.state.selected;
+    let back = this.state.back;
+    let self=this;
+    
+    if(islogged==null & openregister==null)
+      {
+        return(
+          <LoginView 
+          
+            openregister={function(){
+                self.openRegister();
+            }}
 
-    if(movies!=null && selected==null) 
-    {
-      return(
-            <div className='container'>
-            <div className='row'>
-             
-              {movies.map(movie=>
-                <MovieCard key={movie._id}
-                           title={movie.title}
-                           id={movie._id}  
-                           movie={movie}
-                           selected={movie=>this.selectedMovie(movie)}
-                />
-              )} 
-             </div> 
-            </div>  
-      )
-    }
-    else if(movies!=null && selected!=null && back==null)
-    {
-      return (<MovieView
+            openmovies={function(){
+              self.openMovies();
+          }}
 
-              movie={this.state.selected}
-              back={goBack=>this.goBack()}
-              />);
-    }
+          />
+        );
+      }
 
-    else
-    {
-      return('');
-    }
+      if(openregister==true)
+      {
+        return(
+          <RegistrationView backtologin={function(){
+            self.baktoLogin();
+          }} />
+        );
+      }
 
+      else
+      {
+        if(movies!=null && selected==null) 
+        {     
+          return(
+                <div className='container'>
+                <div className='row'>
+                 
+                  {movies.map(movie=>
+                    <MovieCard key={movie._id}
+                               title={movie.title}
+                               id={movie._id}  
+                               movie={movie}
+                               selected={movie=>this.selectedMovie(movie)}
+                    />
+                  )} 
+                 </div> 
+                </div>  
+          )
+        }
+        else if(movies!=null && selected!=null && back==null)
+        {
+          return (<MovieView
+    
+                  movie={this.state.selected}
+                  
+                  back={goBack=>this.goBack()}
+                  />);
+        }
+    
+        else
+        {
+          return('');
+        }
+      }
   }
+  
   componentDidMount(){
     axios.get('https://stavflix.herokuapp.com/movies').then(response=>{
       var data = response.data;
       this.setState({content:data});
+      console.log(this.state.back);
     })
   }
 
+
   selectedMovie(movie){
-    this.setState({selected:movie, back:null});
+    this.setState({selected:movie});
   }
 
+  //go back to movies screen
   goBack(){
     this.setState({selected:null});
     
   }
 
-  
+  //open user register screen
+  openRegister(){
+    
+    this.setState({openRegister:true})
+  }
 
-  
+ //open movies screen after successful login
+  openMovies(){
+    this.setState({isLogged:true})
+    
+    
+  }
+  //go back to login page
+  baktoLogin(){
+    this.setState({isLogged:null, openRegister:null})
+  }
 
 }
 
