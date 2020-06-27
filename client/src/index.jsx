21c47d8ac551662e6a7147609833707e7dddf8a5
skouroufexis <skouroufexis@gmail.com>
2020-06-27 18:17:29 +0300

    Update index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,14 @@
-import './index.scss';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.scss';
+
 
 import {MainView} from './components/main-view/main-view';
 import {MovieCard} from './components/movie-card/movie-card';
 import {MovieView} from './components/movie-view/movie-view';
 
 import { format } from 'morgan';
+
 
 var container = document.getElementById('root');
 
