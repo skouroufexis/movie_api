746126173ddc50beb57c55f0276b24bf7b94cd3f
skouroufexis <skouroufexis@gmail.com>
2020-07-30 13:04:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
     const app=express();
     app.use(express.static('public'));
     // app.use("/client", express.static(path.join(__dirname, "client", "dist")));
-
+    app.use(express.static(path.join(__dirname, "client","src", "dist")));
 
     
 
